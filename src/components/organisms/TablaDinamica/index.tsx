d89c3ca20c72b13feb4c59248a7b7cfc66b9e737
Kevin Nieto <155347741+KevinNieto98@users.noreply.github.com>
2024-06-05 07:56:30 -0600

    Tabla dinamica parte 5

--- src/components/organisms/TablaDinamica/index.tsx
+++ src/components/organisms/TablaDinamica/index.tsx
@@ -1,19 +1,26 @@
 'use client'
 
-import {  CeldaDinamica } from "@/components";
-import {  Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
+import { CeldaDinamica } from "@/components";
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Input, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
+import { useCallback, useMemo, useState } from "react";
+import { FaPlus, FaSearch } from "react-icons/fa";
 
 interface MyObject {
     [key: number]: any;
 }
 
 interface TablaDinamica {
-    data : any[],
-   dinamica :boolean
+    data: any[],
+    dinamica: boolean
 }
 
 
 export const TablaDinamica = ({ data, dinamica }: TablaDinamica) => {
+
+    const [filterValue, setFilterValue] = useState("");
+    const [page, setPage] = useState(1);
+    const [statusFilter, setStatusFilter] = useState("all");
+    const [rowsPerPage, setRowsPerPage] = useState(5);
     let headers: string[] = [];
     let newData: any[] = [];
     if (!data || data.length === 0) {
@@ -30,7 +37,7 @@ export const TablaDinamica = ({ data, dinamica }: TablaDinamica) => {
             newObj[Object.keys(obj).length] = 'accion';
             return newObj;
         });
-    }else{
+    } else {
         headers = [...Object.keys(data[0]).map(key => key.toUpperCase())];
 
         newData = data.map((obj: any) => {
@@ -41,9 +48,77 @@ export const TablaDinamica = ({ data, dinamica }: TablaDinamica) => {
             return newObj;
         });
     }
+    //TODO: Cambiar por HEADERS
+    const [visibleColumns, setVisibleColumns] = useState(new Set(headers));
+
+    const onRowsPerPageChange = useCallback((e: any) => {
+        setRowsPerPage(Number(e.target.value));
+        setPage(1);
+    }, []);
+
+    const onSearchChange = useCallback((value: any) => {
+        if (value) {
+            setFilterValue(value);
+            setPage(1);
+        } else {
+            setFilterValue("");
+        }
+    }, []);
+
+    const onClear = useCallback(() => {
+        setFilterValue("")
+        setPage(1)
+    }, [])
+
+    const topContent = useMemo(() => {
+        return (
+            <div className="flex flex-col gap-4">
+                <div className="flex justify-between gap-3 items-end">
+                    <Input
+                        isClearable
+                        className="w-full sm:max-w-[44%]"
+                        placeholder="Search by name..."
+                        startContent={<FaSearch />}
+                        value={filterValue}
+                        onClear={() => onClear()}
+                        onValueChange={onSearchChange}
+                    />
+                    <div className="flex gap-3">
+                        <Button color="primary" endContent={<FaPlus />}>
+                            Nuevo
+                        </Button>
+                    </div>
+                </div>
+                <div className="flex justify-between items-center">
+                    <span className="text-default-400 text-small">Total: {data.length} tipos</span>
+                    <label className="flex items-center text-default-400 text-small">
+                        Rows per page:
+                        <select
+                            className="bg-transparent outline-none text-default-400 text-small"
+                            onChange={onRowsPerPageChange}
+                        >
+                            <option value="5">5</option>
+                            <option value="10">10</option>
+                            <option value="15">15</option>
+                        </select>
+                    </label>
+                </div>
+            </div>
+        );
+    }, [
+        filterValue,
+        statusFilter,
+        visibleColumns,
+        onRowsPerPageChange,
+        onSearchChange,
+    ]);
+
 
     return (
-        <Table aria-label="Example table with custom cells">
+        <Table
+            aria-label="Example table with custom cells"
+            topContent={topContent}
+        >
             <TableHeader>
                 {headers.map((header, index) => (
                     <TableColumn key={index} align="center">{header}</TableColumn>
